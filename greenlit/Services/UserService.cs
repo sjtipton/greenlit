cc2baffa044dc2bb4ff6f92d1546e14b1e1539d0
Steve Tipton <slaatipton26@gmail.com>
2018-09-25 12:33:50 -0400

    Implement UserService

--- greenlit/Services/UserService.cs
+++ greenlit/Services/UserService.cs
@@ -1,6 +1,8 @@
 ﻿using greenlit.Entities;
+using greenlit.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace greenlit.Services
 {
@@ -15,32 +17,117 @@ namespace greenlit.Services
 
         public User Authenticate(string identifier, string password)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(identifier) || string.IsNullOrEmpty(password))
+                return null;
+
+            var user = _context.Users.SingleOrDefault(x => x.EmailAddress == identifier);
+
+            if (user == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
         }
 
         public IEnumerable<User> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Users;
         }
 
         public User GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Users.Find(id);
         }
 
         public User Create(User user, string password)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(password))
+                throw new AppException("Password is required");
+
+            if (_context.Users.Any(x => x.EmailAddress == user.EmailAddress))
+                throw new AppException($"Email address {user.EmailAddress} is already taken");
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return user;
         }
 
         public void Update(User userParam, string password = null)
         {
-            throw new NotImplementedException();
+            var user = _context.Users.Find(userParam.Id);
+
+            if (user == null)
+                throw new AppException("User not found");
+
+            if (userParam.EmailAddress != user.EmailAddress)
+            {
+                if (_context.Users.Any(x => x.EmailAddress == userParam.EmailAddress))
+                    throw new AppException($"Email address {user.EmailAddress} is already taken");
+            }
+
+            user.FirstName = userParam.FirstName;
+            user.LastName = userParam.LastName;
+            user.EmailAddress = userParam.EmailAddress;
+
+            if (!string.IsNullOrWhiteSpace(password))
+            {
+                CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+            }
+
+            _context.Users.Update(user);
+            _context.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            if (password == null) throw new ArgumentNullException("password");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            if (password == null) throw new ArgumentNullException("password");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+            if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+            if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordSalt");
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != storedHash[i]) return false;
+                }
+            }
+
+            return true;
         }
     }
 }
