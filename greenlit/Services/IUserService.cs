776f7aced4ded6246ad617ea30da9953772d9d45
Steve Tipton <slaatipton26@gmail.com>
2018-09-25 11:23:17 -0400

    Add IUserService and stub NotImplementedExceptions on concrete UserService

---
+++ greenlit/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using greenlit.Entities;
+using System.Collections.Generic;
+
+namespace greenlit.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string identifier, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        User Create(User user, string password);
+        void Update(User user, string password = null);
+        void Delete(int id);
+    }
+}
---
+++ greenlit/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using greenlit.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace greenlit.Services
+{
+    public class UserService : IUserService
+    {
+        private DataContext _context;
+
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public User Authenticate(string identifier, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Create(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User userParam, string password = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
