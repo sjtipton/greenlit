272341c1b0ffaacf9b3646febb4bafb21e508f2e
Steve Tipton <slaatipton26@gmail.com>
2018-09-25 11:20:46 -0400

    Add DataContext and User Entities class

---
+++ greenlit/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace greenlit.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
