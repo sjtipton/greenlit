e6267f62d9431db799db52072970d5bfa616c860
Steve Tipton <slaatipton26@gmail.com>
2018-09-25 11:21:25 -0400

    Add UserDto and AutoMapperProfile helper class

---
+++ greenlit/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using greenlit.Dtos;
+using greenlit.Entities;
+
+namespace greenlit.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
